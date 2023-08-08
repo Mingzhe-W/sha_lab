73d322cef249d41fd427bec5e8d682c45d8403e4
Mingzhe Wang <mingzhe3@illinois.edu>
2023-08-08 16:24:22 +0800

    constants

--- python/sha_256_hash.py
+++ python/sha_256_hash.py
@@ -4,7 +4,7 @@ def sha_256_secure_hash(M):
 
     schedule_arrays = []
     for chunk in M:
-        schedule_array  =[0x00000000] * 64
+        schedule_array  =[bytearray(0x00000000)] * 64
         schedule_arrays.append(schedule_array)
 
     return schedule_arrays
